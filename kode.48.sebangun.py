b1ce2cf80f571162ce4e5ba15b8d5434eb74c893
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-11-22 13:44:15 +0700

    Add files via upload

---
+++ kode.48.sebangun.py
@@ -0,0 +1,19 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+
+# Menginput
+print('Isi nilai-nilai sisi besar dan kecil ')
+a = int(input("Masukan Nilai A (Kecil 1) : "))
+b = int(input("Masukan Nilai B (Kecil 2) : "))
+c = int(input("Masukan Nilai C (Besar 1) : "))
+
+# Mengkonversi
+d= b * c / a
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Hasilnya : C/A = D/B ')
+print('Maka Nilai D (Besar 2) = ',d)
+print('==========================================================')
